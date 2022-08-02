4aacd280cd0885acfd7622c463f2955447f5e2ca
robertoQ <nececuentag5tobiosep@gmail.com>
2022-08-02 13:55:01 -0500

    Templates

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home page</h1>')
+    searchTerm = request.GET.get('SearchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
