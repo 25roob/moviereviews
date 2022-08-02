c6f7c0aa62705644d2cbc8de9343e09cb1452306
robertoQ <nececuentag5tobiosep@gmail.com>
2022-08-02 17:51:25 -0500

    implementing_bootstrap

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def home(request):
-    searchTerm = request.GET.get('SearchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About page</h1>')
